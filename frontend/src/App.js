7484ee5891d96916047eb54e9cd59f83eede1710
nvquynh29 <nvquynh29@gmail.com>
2024-02-29 23:38:27 +0900

    Configure TailwindCSS build

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,22 +5,7 @@ import TodoListHeading from './components/TodoListHeading'
 
 const TodoApp = () => {
   const [todoList, setTodoList] = useState([])
-
-  // const getTodoList = async () => {
-  //   return fetch(process.env.REACT_APP_API_ENDPOINT)
-  //     .then((res) => res.json())
-  //     .then((data) => setTodoList(data))
-  //     .catch((error) => {
-  //       console.error(error)
-  //       return []
-  //     })
-  // }
-
-  // useEffect(() => {
-  //   getTodoList()
-  // }, [])
   
-  console.log(todoList)
   return (
     <TodoProvider initialTodos={todoList}>
       <div className="container mx-auto max-w-3xl flex flex-col h-fit max-h-screen justify-center py-10 px-5 md:px-2">
